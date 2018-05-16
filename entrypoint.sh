124db2e70c984f1f1b5412f36990f4c0de432646
KennyBZA <kblewett88@gmail.com>
2018-05-16 15:05:06 +0200

    Update entrypoint.sh
    Minor alterations

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,8 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 ./steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous +force_install_dir /home/container +app_update 824360 +quit
 
 # Set Wine architechture 
-#export wine wineboot -i
+export WINEARCH='win64'
+wine wineboot --init
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
